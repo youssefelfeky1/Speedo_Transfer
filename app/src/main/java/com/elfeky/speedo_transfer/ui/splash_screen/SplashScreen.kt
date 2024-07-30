d44f1a721079b92f8a6cba544cb74717ceb5b02f
Youssef_Elfeky <youssefelfeky83@gmail.com>
2024-07-30 12:42:43 +0300

    Splash Screen

---
+++ app/src/main/java/com/elfeky/speedo_transfer/ui/splash_screen/SplashScreen.kt
@@ -0,0 +1,65 @@
+package com.elfeky.speedo_transfer.ui.splash_screen
+
+
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.elfeky.speedo_transfer.ui.theme.DarkRed
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavController){
+    val scale = remember {
+        Animatable(0f)
+    }
+    LaunchedEffect(key1 = true){
+        scale.animateTo(
+            targetValue = 0.8f,
+            animationSpec = tween(
+                durationMillis = 700,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
+        delay(3000L)
+        navController.navigate("signIn_screen")
+    }
+
+    Box(
+        modifier =Modifier
+            .fillMaxSize()
+            .background(DarkRed),
+        contentAlignment = Alignment.Center
+    ){
+        Text(
+            text ="Speedo Transfer",
+            fontSize = 36.sp,
+            color = Color.White,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.scale(scale.value)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SplashScreenPreview() {
+    SplashScreen(navController = rememberNavController())
+}
