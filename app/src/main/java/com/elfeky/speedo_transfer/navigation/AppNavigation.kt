5e43988cc47e6c1cfe3c469826805077305cf14b
Youssef_Elfeky <youssefelfeky83@gmail.com>
2024-08-02 00:05:53 +0300

    Update Navigation

--- app/src/main/java/com/elfeky/speedo_transfer/navigation/AppNavigation.kt
+++ app/src/main/java/com/elfeky/speedo_transfer/navigation/AppNavigation.kt
@@ -13,19 +13,24 @@ import com.elfeky.speedo_transfer.ui.authentication.SignUp
 import com.elfeky.speedo_transfer.ui.main_screen.MainScreen
 import com.elfeky.speedo_transfer.ui.on_boarding.OnboardingNavigation
 import com.elfeky.speedo_transfer.ui.splash_screen.SplashScreen
+import com.elfeky.speedo_transfer.util.Constants.EXTENDED_SIGN_UP
+import com.elfeky.speedo_transfer.util.Constants.ONBOARD
+import com.elfeky.speedo_transfer.util.Constants.SIGN_IN
+import com.elfeky.speedo_transfer.util.Constants.SIGN_UP
+import com.elfeky.speedo_transfer.util.Constants.SPLASH_SCREEN
 
 @Composable
 fun AppNavigation(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "SplashScreen") {
-        composable("SplashScreen") {
+    NavHost(navController = navController, startDestination = SPLASH_SCREEN) {
+        composable(SPLASH_SCREEN) {
             SplashScreen(navController = navController)
         }
-        composable("SignUp") {
+        composable(SIGN_UP) {
             SignUp(navController = navController)
         }
         composable(
-            "ExtendedSignUp/{name}/{email}/{password}",
+            "$EXTENDED_SIGN_UP/{name}/{email}/{password}",
             arguments = listOf(
                 navArgument("name") { type = NavType.StringType },
                 navArgument("email") { type = NavType.StringType },
@@ -42,10 +47,10 @@ fun AppNavigation(modifier: Modifier = Modifier) {
                 navController = navController
             )
         }
-        composable("SignIn") {
+        composable(SIGN_IN) {
             SignIn(navController = navController)
         }
-        composable("Onboard") {
+        composable(ONBOARD) {
             val navController2 = rememberNavController()
             OnboardingNavigation(navController = navController2, navController)
         }
