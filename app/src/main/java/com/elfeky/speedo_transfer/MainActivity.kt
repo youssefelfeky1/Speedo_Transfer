a2b2cea0b0596c4752af39d88682e31dcf284afd
Youssef_Elfeky <youssefelfeky83@gmail.com>
2024-08-02 04:23:31 +0300

    Clean Project

--- app/src/main/java/com/elfeky/speedo_transfer/MainActivity.kt
+++ app/src/main/java/com/elfeky/speedo_transfer/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.elfeky.speedo_transfer.navigation.AppNavigation
-import com.elfeky.speedo_transfer.ui.authentication.ExtendedSignUp
-import com.elfeky.speedo_transfer.ui.main_screen.more.profile.ProfileScreen
-import com.elfeky.speedo_transfer.ui.main_screen.more.profile.settings.EditProfile
-import com.elfeky.speedo_transfer.ui.on_boarding.OnboardingNavigation
 import com.elfeky.speedo_transfer.ui.theme.Speedo_TransferTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Speedo_TransferTheme {
-//               AppNavigation()
-//                ExtendedSignUp(name = "", email = "", password = "", navController = rememberNavController())
-                EditProfile()
+                AppNavigation()
             }
         }
     }
