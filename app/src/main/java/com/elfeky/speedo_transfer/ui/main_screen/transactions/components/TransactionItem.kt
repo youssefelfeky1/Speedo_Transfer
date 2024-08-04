9d47527ee41e7b9c79069b04fb3705d5fa696ab4
Youssef_Elfeky <youssefelfeky83@gmail.com>
2024-08-04 04:31:33 +0300

    clean project

--- app/src/main/java/com/elfeky/speedo_transfer/ui/main_screen/transactions/components/TransactionItem.kt
+++ app/src/main/java/com/elfeky/speedo_transfer/ui/main_screen/transactions/components/TransactionItem.kt
@@ -6,13 +6,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
