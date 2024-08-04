ce0bc54532e46be4ba981243075cfe597815f5b5
AmrNasser <amr931111@gmail.com>
2024-08-05 01:02:09 +0300

    salee  3ala elnaby  - enhance ui

--- app/src/main/java/com/elfeky/speedo_transfer/ui/main_screen/more/components/LogoutDialog.kt
+++ app/src/main/java/com/elfeky/speedo_transfer/ui/main_screen/more/components/LogoutDialog.kt
@@ -1,9 +1,22 @@
 package com.elfeky.speedo_transfer.ui.main_screen.more.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.elfeky.speedo_transfer.ui.theme.DarkRed
+import com.elfeky.speedo_transfer.ui.theme.RoseBottomGradient
+
 
 @Composable
 fun AlertDialogLogout(
@@ -11,22 +24,39 @@ fun AlertDialogLogout(
     onConfirm: () -> Unit
 ) {
     AlertDialog(
+        containerColor = RoseBottomGradient,
         onDismissRequest = onDismiss,
-        title = {
-            Text(text = "Logout")
-        },
         text = {
-            Text("Are you sure you want to logout?")
+            Text("Are you sure you want to logout?", color = DarkRed)
         },
         confirmButton = {
-            Button(onClick = onConfirm) {
-                Text("Confirm")
+            Button(
+                onClick = onConfirm,
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                   ,
+                colors = ButtonDefaults.buttonColors(DarkRed)
+
+
+            ) {
+                Text("Confirm", color = Color.White)
             }
         },
         dismissButton = {
-            Button(onClick = onDismiss) {
-                Text("Dismiss")
+            OutlinedButton(
+                onClick = onDismiss,
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .background(RoseBottomGradient)
+            ) {
+                Text("Dismiss", color = DarkRed)
             }
         }
     )
+}
+
+@Preview
+@Composable
+private fun AlertDialogLogoutPreview() {
+    AlertDialogLogout(onConfirm = {}, onDismiss = {})
 }
